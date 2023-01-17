482ce9da615089789338a0b2094483c7d84643dc
BedinoTom <tom.bedino@etu.univ-cotedazur.fr>
2023-01-17 13:12:33 +0100

    test: Add some label in test file #11

--- test/1-4_instructions_label.s
+++ test/1-4_instructions_label.s
@@ -4,6 +4,7 @@ movs r0, #0 @tom
 movs r1, #1
 movs r2, #170
 movs r3,#255
+wgile:
     movs r4,#47
 lsls r4, r2, #1
 @r4 value should be 340, 154 
@@ -11,6 +12,7 @@ lsls r4, r2, #1
 lsrs r5, r2, #1
     @r5 value should be 85, 55 
 
+if:
 subs r6, r0, #5
 asrs r6, r6, #1
 @r6 value should be -3 ou FFFFFFFD
